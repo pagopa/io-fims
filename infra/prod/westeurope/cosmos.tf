a205a4ae3fbd7abdbc775ceec09cb5409ded1ee6
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-11 13:06:40 +0100

    test other tf verresion

--- infra/prod/westeurope/cosmos.tf
+++ infra/prod/westeurope/cosmos.tf
@@ -1,7 +1,7 @@
 module "cosmos" {
   source = "../_modules/cosmos"
 
-  location            = local.location
+  location            = module.resource_groups.resource_group_fims.location
   project             = local.project
   product             = local.product
   resource_group_name = module.resource_groups.resource_group_fims.name
