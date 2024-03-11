911ab999464acd6651beb96e86254b792824721a
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-11 12:49:39 +0100

    update state file name

--- infra/prod/westeurope/main.tf
+++ infra/prod/westeurope/main.tf
@@ -4,7 +4,7 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfappprodio"
     container_name       = "terraform-state"
-    key                  = "io-fims.fims.tfstate"
+    key                  = "io-fims.infra.tfstate"
   }
 
   required_providers {
