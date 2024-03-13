0c5ebc954d64643154e1c89ac7d41cbf7fa8d07d
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-13 10:18:22 +0100

    add link

--- infra/prod/westeurope/networking.tf
+++ infra/prod/westeurope/networking.tf
@@ -4,7 +4,9 @@ module "networking" {
   location = local.location
   project  = local.project
 
-  subnet_fims_cidr = "10.0.18.0/26" # inferred from vnet-common with cidr 10.0.0.0/16
+  # inferred from vnet-common with cidr 10.0.0.0/16
+  # https://github.com/pagopa/io-infra/blob/2a74355cdf70bf965b0c8900983bfc923c83ebfb/src/core/network.tf#L8
+  subnet_fims_cidr = "10.0.18.0/26"
 
   tags = local.tags
 }
