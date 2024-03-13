574eda43560787f475dcfd2b2a91eaf87a745e28
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-13 09:54:50 +0100

    add doc

--- infra/prod/westeurope/networking.tf
+++ infra/prod/westeurope/networking.tf
@@ -4,7 +4,7 @@ module "networking" {
   location = local.location
   project  = local.project
 
-  subnet_fims_cidr = "10.0.18.0/26"
+  subnet_fims_cidr = "10.0.18.0/26" # inferred from vnet-common with cidr 10.0.0.0/16
 
   tags = local.tags
 }
