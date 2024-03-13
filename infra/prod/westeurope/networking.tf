6d137c78e9139d275f051d6ccf2b898be86dc50f
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-13 10:58:51 +0100

    [EC-226] Add inline doc to FIMS cidr definition (#12)

--- infra/prod/westeurope/networking.tf
+++ infra/prod/westeurope/networking.tf
@@ -4,6 +4,8 @@ module "networking" {
   location = local.location
   project  = local.project
 
+  # inferred from vnet-common with cidr 10.0.0.0/16
+  # https://github.com/pagopa/io-infra/blob/2a74355cdf70bf965b0c8900983bfc923c83ebfb/src/core/network.tf#L8
   subnet_fims_cidr = "10.0.18.0/26"
 
   tags = local.tags
