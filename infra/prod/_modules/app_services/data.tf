8bd4ecd6979d7c464ac02b2613e576ec94355d53
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-14 16:37:50 +0100

    add private endpoint

--- infra/prod/_modules/app_services/data.tf
+++ infra/prod/_modules/app_services/data.tf
@@ -26,6 +26,17 @@ data "azurerm_subnet" "subnet_azdoa" {
   resource_group_name  = data.azurerm_virtual_network.vnet_common.resource_group_name
 }
 
+data "azurerm_subnet" "private_endpoints_subnet" {
+  name                 = "pendpoints"
+  virtual_network_name = data.azurerm_virtual_network.vnet_common.name
+  resource_group_name  = data.azurerm_virtual_network.vnet_common.resource_group_name
+}
+
+data "azurerm_private_dns_zone" "privatelink_azurewebsites_net" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = local.resource_group_name_common
+}
+
 data "azurerm_key_vault_secret" "cookies_key_fims" {
   name         = "${var.project}-fims-cookies-key"
   key_vault_id = var.key_vault_id
