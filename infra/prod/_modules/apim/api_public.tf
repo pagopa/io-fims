c09495dee25e0adec7a83048636330f33cc1ea5b
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-14 15:40:03 +0100

    wip

--- infra/prod/_modules/apim/api_public.tf
+++ infra/prod/_modules/apim/api_public.tf
@@ -1,4 +1,4 @@
-module "apim_product_fims_public" {
+module "apim_product_fims_openid_provider" {
   source = "github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v7.67.1"
 
   product_id          = "io-fims-openid-provider-api"
@@ -22,12 +22,12 @@ module "apim_api_fims_public" {
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
 
   revision     = "1"
-  display_name = "FIMS PUBLIC API"
-  description  = "PUBLIC API for FIMS."
+  display_name = "IO FIMS OPENID PROVIDER"
+  description  = "Public API for FIMS OpenId Provider."
 
   path        = "fims"
   protocols   = ["https"]
-  product_ids = [module.apim_product_fims_public.product_id]
+  product_ids = [module.apim_product_fims_openid_provider.product_id]
 
   service_url = format("https://%s", var.app_service_oicd_provider_hostname)
 
--- infra/prod/westeurope/apim.tf
+++ infra/prod/westeurope/apim.tf
@@ -4,5 +4,5 @@ module "apim" {
   location = module.resource_groups.resource_group_fims.location
   project  = local.project
 
-  app_service_oicd_provider_hostname = ""
+  app_service_oicd_provider_hostname = "io-p-fims-openid-provider-app.azurewebsites.net"
 }
