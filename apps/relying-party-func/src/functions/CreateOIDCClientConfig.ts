d6f36baf65a94150bc4a420a3327962402d15ad0
garma00 <giovanniarmaleo00@gmail.com>
2024-04-02 17:17:05 +0200

    Add healthcheck function

---
+++ apps/relying-party-func/src/functions/CreateOIDCClientConfig.ts
@@ -0,0 +1,12 @@
+import { app, HttpResponseInit } from "@azure/functions";
+
+export const healthcheckHandler = async (): Promise<HttpResponseInit> => ({
+  status: 200,
+  body: "Function up and running",
+});
+
+app.http("httpTrigger1", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: healthcheckHandler,
+});
