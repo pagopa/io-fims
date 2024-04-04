1e8687560858c66f52220517d1e6f84316aeb47f
garma00 <giovanniarmaleo00@gmail.com>
2024-04-04 11:43:13 +0200

    Add type definitions

---
+++ apps/openid-rp-example-app/src/types/index.d.ts
@@ -0,0 +1,9 @@
+declare module "openid-client" {
+  export type AuthorizationParameters = {};
+  export type IdTokenClaims = {};
+  export type UserinfoResponse = {};
+}
+declare module "jose" {
+  export type JSONWebKey = {};
+  export type KeyInput = {};
+}
