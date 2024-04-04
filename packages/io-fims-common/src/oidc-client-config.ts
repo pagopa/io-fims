4facafcdd072164c2ff38ab33eca48bed623a59d
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-04-04 17:55:51 +0200

    refactor oidc-client-config to make easier

--- packages/io-fims-common/src/oidc-client-config.ts
+++ packages/io-fims-common/src/oidc-client-config.ts
@@ -5,16 +5,12 @@ const callbackSchema = z.object({
   displayName: z.string().min(3).max(15),
 });
 
-export const oidcClientConfigSchema = z.intersection(
-  z.object({
-    id: z.string().ulid(),
-    institutionId: z.string().uuid(),
-    callbacks: z.array(callbackSchema).min(1),
-    scopes: z.tuple([z.literal("openid"), z.literal("profile")]),
-  }),
-  z.discriminatedUnion("for", [
-    z.object({ for: z.literal("service"), serviceId: z.string().ulid() }),
-  ]),
-);
+export const oidcClientConfigSchema = z.object({
+  id: z.string().ulid(),
+  institutionId: z.string().uuid(),
+  callbacks: z.array(callbackSchema).min(1),
+  scopes: z.tuple([z.literal("openid"), z.literal("profile")]),
+  serviceId: z.string().ulid(),
+});
 
 export type OIDCClientConfig = z.TypeOf<typeof oidcClientConfigSchema>;
