3cc242dc743cbc60b94c0274dbe89fdb2fc5389c
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-04-08 12:30:52 +0200

    change workspace name

--- infra/prod/_modules/functions/private_endpoint_relying_party.tf
+++ infra/prod/_modules/functions/private_endpoint_relying_party.tf
@@ -6,7 +6,7 @@ resource "azurerm_private_endpoint" "relying_party" {
   subnet_id           = data.azurerm_subnet.private_endpoints_subnet.id
 
   private_service_connection {
-    name                           = "${var.product}-relying-party-app-endpoint"
+    name                           = "${var.product}-rp-func-endpoint"
     private_connection_resource_id = module.relying_party_func.id
     is_manual_connection           = false
     subresource_names              = ["sites"]
