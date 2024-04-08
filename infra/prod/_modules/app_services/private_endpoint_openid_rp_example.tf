b57e1bb6d7d371192de73d31e884bc8b9c38d93f
garma00 <giovanniarmaleo00@gmail.com>
2024-04-08 14:33:06 +0200

    Rename service

--- infra/prod/_modules/app_services/private_endpoint_openid_rp_example.tf
+++ infra/prod/_modules/app_services/private_endpoint_openid_rp_example.tf
@@ -1,11 +1,11 @@
 resource "azurerm_private_endpoint" "openid_rp_example" {
-  name                = "${var.product}-openid-rp-example-app-endpoint"
+  name                = "openid-rp-example-endpoint"
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_id           = data.azurerm_subnet.private_endpoints_subnet.id
 
   private_service_connection {
-    name                           = "${var.product}-openid-rp-example-app-endpoint"
+    name                           = "openid-rp-example-endpoint"
     private_connection_resource_id = module.appservice_openid_rp_example.id
     is_manual_connection           = false
     subresource_names              = ["sites"]
