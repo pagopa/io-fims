24bbb632ceb0f2f03d8f560bbbe987deb9e6999e
Andrea Grillo <andrea.grillo@pagopa.it>
2024-04-24 14:47:19 +0200

    restore

--- infra/resources/prod/westeurope/apim.tf
+++ infra/resources/prod/westeurope/apim.tf
@@ -1,8 +1,8 @@
 module "apim" {
   source = "../_modules/apim"
 
-  # location = module.resource_groups.resource_group_fims.location
-  project = local.project
+  location = module.resource_groups.resource_group_fims.location
+  project  = local.project
 
   app_service_oicd_provider_hostname = module.web_apps.app_service_opendid_provider.hostname
 }
