506cf5219e0a88a5b0a8d4c1e57ab39e8e39b40f
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-05-20 12:59:07 +0200

    chore(infra): remove old fims resources

--- infra/resources/prod/_modules/cosmos/outputs.tf
+++ infra/resources/prod/_modules/cosmos/outputs.tf
@@ -1,16 +1,3 @@
-output "cosmos_fims" {
-  value = {
-    account = {
-      id   = module.cosmosdb_account_fims.id
-      name = module.cosmosdb_account_fims.name
-    }
-    database = {
-      id   = module.cosmosdb_database_fims.id
-      name = module.cosmosdb_database_fims.name
-    }
-  }
-}
-
 output "account_name" {
   value = module.cosmosdb_account_fims.name
 }
