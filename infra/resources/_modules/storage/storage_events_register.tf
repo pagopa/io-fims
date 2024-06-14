12cc33c3b3a52db0fa7f1903dc7099e499d7cdd0
acuiuli <andrea.cuiuli@gmail.com>
2024-06-14 11:14:34 +0200

    fixed event register storage infra

--- infra/resources/_modules/storage/storage_events_register.tf
+++ infra/resources/_modules/storage/storage_events_register.tf
@@ -4,17 +4,24 @@ resource "azurerm_resource_group" "events_rg" {
 }
 
 resource "azurerm_storage_account" "events_sa" {
-  name                     = "fimseventssa"
-  resource_group_name      = azurerm_resource_group.events_rg.name
-  location                 = azurerm_resource_group.events_rg.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  access_tier              = "Hot"
-  account_replication_type = "ZRS"
-  min_tls_version          = "TLS1_2"
+  name                        = "fimseventssa"
+  resource_group_name           = azurerm_resource_group.events_rg.name
+  location                      = azurerm_resource_group.events_rg.location
+  account_kind                  = "StorageV2"
+  account_tier                  = "Standard"
+  access_tier                   = "Hot"
+  account_replication_type      = "ZRS"
+  min_tls_version               = "TLS1_2"
+  public_network_access_enabled = false
 
   blob_properties {
     versioning_enabled = true
+  }
+
+  immutability_policy {
+    allow_protected_append_writes = true
+    period_since_creation_in_days = 730
+    state                         = "Locked"
   }
 }
 
@@ -22,11 +29,4 @@ resource "azurerm_storage_container" "events_sc" {
   name                  = "fimseventsblob"
   storage_account_name  = azurerm_storage_account.events_sa.name
   container_access_type = "private"
-}
-
-resource "azurerm_storage_container_immutability_policy" "events_policy" {
-  storage_container_resource_manager_id = azurerm_storage_container.events_sc.resource_manager_id
-  immutability_period_in_days           = 24 * 30
-  protected_append_writes_all_enabled   = false
-  protected_append_writes_enabled       = true
 }
