5f51ffe22e755ffc72e87c8dc6d8525748fc55b0
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-06-16 17:36:28 +0200

    feat(common): add health handler

---
+++ packages/io-fims-common/src/adapters/handlers/health.ts
@@ -0,0 +1,25 @@
+import { HealthUseCase } from "@/use-cases/health.js";
+import * as E from "fp-ts/lib/Either.js";
+import * as TE from "fp-ts/lib/TaskEither.js";
+import * as RTE from "fp-ts/lib/ReaderTaskEither.js";
+
+import { pipe } from "fp-ts/lib/function.js";
+
+import * as H from "@pagopa/handler-kit";
+import { logErrorAndReturnResponse } from "@/response.js";
+
+export const health = () => (r: { health: HealthUseCase }) =>
+  TE.tryCatch(() => r.health.execute(), E.toError);
+
+export default H.of(() =>
+  pipe(
+    health(),
+    RTE.map((failures) =>
+      pipe(
+        H.successJson(failures),
+        H.withStatusCode(failures.length > 0 ? 503 : 200),
+      ),
+    ),
+    RTE.orElseW(logErrorAndReturnResponse),
+  ),
+);
---
+++ packages/io-fims-common/src/adapters/handlers/tests/health.spec.ts
@@ -0,0 +1,67 @@
+import { HealthUseCase } from "@/use-cases/health.js";
+import { describe, expect, test } from "vitest";
+
+import * as H from "@pagopa/handler-kit";
+
+import Health from "../health.js";
+
+const success = async () => {};
+const failure = async () => {
+  throw new Error("failure");
+};
+
+const logger = {
+  log: () => () => {},
+};
+
+describe("Health", () => {
+  test.each([
+    {
+      failures: [],
+      input: [],
+      status: 200,
+    },
+    {
+      failures: ["test-1-failure"],
+      input: [
+        { health: success, id: "test-1-ok" },
+        { health: failure, id: "test-1-failure" },
+      ],
+      status: 503,
+    },
+    {
+      failures: [],
+      input: [
+        { health: success, id: "test-2-ok" },
+        { health: success, id: "test-3-ok" },
+      ],
+      status: 200,
+    },
+    {
+      failures: ["test-2-failure"],
+      input: [{ health: failure, id: "test-2-failure" }],
+      status: 503,
+    },
+  ])(
+    "Respond with the right status code ($probes.length probes, $status)",
+    async ({ failures, input, status }) => {
+      const run = Health({
+        logger,
+        health: new HealthUseCase(input),
+        input: H.request("http://my-unit-test.local/health"),
+        inputDecoder: H.HttpRequest,
+      });
+      await expect(run()).resolves.toEqual(
+        expect.objectContaining({
+          right: expect.objectContaining({
+            headers: expect.objectContaining({
+              "Content-Type": "application/json",
+            }),
+            body: failures,
+            statusCode: status,
+          }),
+        }),
+      );
+    },
+  );
+});
