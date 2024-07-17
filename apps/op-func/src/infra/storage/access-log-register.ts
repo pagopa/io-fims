9d1d0efc608af84b6d97a745f5424529bf0c5a02
acuiuli <andrea.cuiuli@gmail.com>
2024-07-17 10:13:11 +0200

    resolved small issue with the access-log-register

--- apps/op-func/src/infra/storage/access-log-register.ts
+++ apps/op-func/src/infra/storage/access-log-register.ts
@@ -3,14 +3,8 @@ import {
   BlockBlobUploadResponse,
   ContainerClient,
 } from "@azure/storage-blob";
-
-interface AccessLogRecord {
-  fiscalCode: string;
-  idToken: string;
-  ipAddress: string;
-  rpAuthRequest: string;
-  timestamp: string;
-}
+import { AuditEvent, auditEventSchema } from "io-fims-common/domain/audit-event";
+import * as assert from "node:assert/strict";
 
 export class AccessLogRegister {
   #container: ContainerClient;
@@ -21,39 +15,42 @@ export class AccessLogRegister {
 
   // A helper method used to read a Node.js readable stream into a String
   async #streamToBuffer(readableStream: NodeJS.ReadableStream) {
-    return new Promise<AccessLogRecord>((resolve, reject) => {
-      const chunks: Buffer[] | Uint8Array[] = [];
-      readableStream.on("data", (data) => {
-        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-      });
-      readableStream.on("end", () => {
-        resolve(JSON.parse(Buffer.concat(chunks).toString()));
-      });
-      readableStream.on("error", reject);
+    return new Promise<AuditEvent>(async (resolve, reject) => {
+      let result = '';
+      for await (const chunk of readableStream) {
+        result += chunk;
+      }
+
+      try {
+        const parsedResult = JSON.parse(result);
+        resolve(auditEventSchema.parse(parsedResult));
+      } catch(e) {
+        reject(e);
+      }
     });
   }
 
-  async get(name: string): Promise<AccessLogRecord> {
+  async get(name: string): Promise<AuditEvent> {
     const blobClient = this.#container.getBlobClient(name);
 
     // Get blob content from position 0 to the end
     // get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
     const downloadBlockBlobResponse = await blobClient.download();
     const downloadedStream = downloadBlockBlobResponse.readableStreamBody;
-    if (!downloadedStream)
-      throw new Error("No Blob with name " + name + " found");
-
-    return await this.#streamToBuffer(downloadedStream);
+    assert.ok(
+      downloadedStream,
+      "No Blob with name " + name + " found",
+    );
+    return this.#streamToBuffer(downloadedStream);
   }
 
   async upload(
-    name: string,
-    content: AccessLogRecord,
+    content: AuditEvent,
   ): Promise<BlockBlobUploadResponse> {
-    const blockBlobClient = this.#container.getBlockBlobClient(name);
+    const blockBlobClient = this.#container.getBlockBlobClient(content.blobName);
     try {
       const parsedContent = JSON.stringify(content);
-      return await blockBlobClient.upload(parsedContent, parsedContent.length);
+      return blockBlobClient.upload(parsedContent, parsedContent.length);
     } catch (e) {
       throw new Error("Error creating new blob", {
         cause: e,
---
+++ packages/io-fims-common/src/domain/audit-event.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+import { userMetadataSchema } from "./user-metadata.js";
+
+export const rpParamsSchema = z.object({
+  client_id: z.string().min(1),
+  redirect_uri: z.string().min(1).optional(),
+  response_type: z.string().min(1).optional(),
+  scope: z.string().min(1).optional(),
+});
+
+export type RPParams = z.TypeOf<typeof rpParamsSchema>;
+
+export const auditEventSchema = z.object({
+  blobName: z.string().min(1),
+  idToken: z.string().min(1).optional(),
+  ipAddress: z.string().min(1).optional(),
+  rpParams: rpParamsSchema.optional(),
+  userData: userMetadataSchema.optional(),
+  timestamp: z.number().optional()
+});
+
+export type AuditEvent = z.TypeOf<typeof auditEventSchema>;
