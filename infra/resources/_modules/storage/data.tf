8d89a121e13194e696611c72f4af832d9ce424f6
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 10:45:02 +0100

    CES-466-migrate-iopfimsst private endpoint added

--- infra/resources/_modules/storage/data.tf
+++ infra/resources/_modules/storage/data.tf
@@ -2,3 +2,14 @@ data "azurerm_private_dns_zone" "privatelink_queue_core_windows_net" {
   name                = "privatelink.queue.core.windows.net"
   resource_group_name = var.virtual_network.resource_group_name
 }
+
+data "azurerm_virtual_network" "vnet_itn" {
+  name                = "${var.project}-itn-common-vnet-01"
+  resource_group_name = "${var.project}-itn-common-rg-01"
+}
+
+data "azurerm_subnet" "subnet_private_endpoints_itn" {
+  name                 = "io-p-itn-pep-snet-01 "
+  resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
+}
