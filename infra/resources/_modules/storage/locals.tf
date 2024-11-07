35ea85e734bae869b8a0d52592023681454638f8
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 11:15:23 +0100

    CES-466-migrate-iopfimsst app_name added

--- infra/resources/_modules/storage/locals.tf
+++ infra/resources/_modules/storage/locals.tf
@@ -2,11 +2,13 @@
 locals {
   prefix          = "io"
   env_short       = "p"
+  app_name        = ""
   domain          = "fims"
   instance_number = "01"
   itn_environment = {
     prefix          = local.prefix
     env_short       = local.env_short
+    app_name        = local.app_name
     location        = var.location
     domain          = local.domain
     instance_number = local.instance_number
