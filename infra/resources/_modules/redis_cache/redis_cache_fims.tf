1fa72af4154d98e148e69f7d47ca88cd00d7291c
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-01-24 16:38:47 +0100

    chore(infra): scale up redis

--- infra/resources/_modules/redis_cache/redis_cache_fims.tf
+++ infra/resources/_modules/redis_cache/redis_cache_fims.tf
@@ -2,7 +2,7 @@ resource "azurerm_redis_cache" "fims" {
   name                          = "${local.project}-redis-01"
   location                      = var.environment.location
   resource_group_name           = var.resource_group_name
-  capacity                      = 0
+  capacity                      = 2
   family                        = "C"
   sku_name                      = "Basic"
   enable_non_ssl_port           = false
