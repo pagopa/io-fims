71b8260e0766518aaa4877a2b098dd20717e2cf0
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-04-04 14:29:42 +0000

    Remove unused resources

--- infra/resources/_modules/web_apps/variables.tf
+++ infra/resources/_modules/web_apps/variables.tf
@@ -16,26 +16,6 @@ variable "tags" {
   type = map(any)
 }
 
-variable "virtual_network" {
-  type = object({
-    name                = string
-    resource_group_name = string
-  })
-}
-
-variable "subnet_pep_id" {
-  type = string
-}
-
-variable "subnet_cidrs" {
-  type = object({
-    op_app     = string
-    op_func    = string
-    user_func  = string
-    rp_example = string
-  })
-}
-
 variable "key_vault" {
   type = object({
     id        = string
@@ -43,68 +23,3 @@ variable "key_vault" {
     vault_uri = string
   })
 }
-
-variable "key_vault_common" {
-  type = object({
-    id   = string
-    name = string
-  })
-}
-
-
-variable "cosmosdb_account" {
-  type = object({
-    name                = string
-    resource_group_name = string
-  })
-}
-
-variable "storage" {
-  type = object({
-    id                  = string
-    name                = string
-    resource_group_name = string
-    queues = object({
-      audit_events = object({
-        name = string
-      })
-      config = object({
-        name = string
-      })
-      access = object({
-        name = string
-      })
-      export = object({
-        name = string
-      })
-    })
-  })
-}
-
-variable "audit_storage" {
-  type = object({
-    id                  = string
-    name                = string
-    resource_group_name = string
-    containers = object({
-      events = object({
-        name = string
-      })
-    })
-  })
-}
-
-variable "redis_cache" {
-  type = object({
-    id         = string
-    url        = string
-    access_key = string
-  })
-}
-
-variable "application_insights" {
-  type = object({
-    connection_string = string
-  })
-}
-
