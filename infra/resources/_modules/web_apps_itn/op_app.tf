f7757c85e95bc68d2bed626038de4b7ebbd95344
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-04-04 14:41:30 +0000

    Add autoscaler to op and user services

--- infra/resources/_modules/web_apps_itn/op_app.tf
+++ infra/resources/_modules/web_apps_itn/op_app.tf
@@ -116,3 +116,24 @@ resource "azurerm_cosmosdb_sql_role_assignment" "op_app_slot" {
   principal_id        = module.op_app.app_service.app_service.slot.principal_id
   scope               = data.azurerm_cosmosdb_account.fims.id
 }
+
+module "op_autoscaler" {
+  source  = "pagopa-dx/azure-app-service-plan-autoscaler/azurerm"
+  version = "~> 1.0"
+
+  resource_group_name = var.resource_group_name
+  location            = var.environment.location
+
+  app_service_plan_id = module.op_app.app_service.plan.id
+
+  target_service = {
+    function_app = {
+      name = module.op_func.function_app.function_app.name
+    },
+    app_service = {
+      name = module.op_app.app_service.app_service.name
+    }
+  }
+
+  tags = var.tags
+}
