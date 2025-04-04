f1bf0d240462661f85cc1c37bd1808aa029650ff
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-04-04 14:46:26 +0000

    Refactor autoscaler module names and add autoscaler for function app

--- infra/resources/_modules/web_apps_itn/op_app.tf
+++ infra/resources/_modules/web_apps_itn/op_app.tf
@@ -117,7 +117,7 @@ resource "azurerm_cosmosdb_sql_role_assignment" "op_app_slot" {
   scope               = data.azurerm_cosmosdb_account.fims.id
 }
 
-module "op_autoscaler" {
+module "op_app__autoscaler" {
   source  = "pagopa-dx/azure-app-service-plan-autoscaler/azurerm"
   version = "~> 1.0"
 
@@ -127,9 +127,6 @@ module "op_autoscaler" {
   app_service_plan_id = module.op_app.app_service.plan.id
 
   target_service = {
-    function_app = {
-      name = module.op_func.function_app.function_app.name
-    },
     app_service = {
       name = module.op_app.app_service.app_service.name
     }
--- infra/resources/_modules/web_apps_itn/op_func.tf
+++ infra/resources/_modules/web_apps_itn/op_func.tf
@@ -72,3 +72,21 @@ resource "azurerm_cosmosdb_sql_role_assignment" "op_func" {
   principal_id        = module.op_func.function_app.function_app.principal_id
   scope               = data.azurerm_cosmosdb_account.fims.id
 }
+
+module "op_func_autoscaler" {
+  source  = "pagopa-dx/azure-app-service-plan-autoscaler/azurerm"
+  version = "~> 1.0"
+
+  resource_group_name = var.resource_group_name
+  location            = var.environment.location
+
+  app_service_plan_id = module.op_app.app_service.plan.id
+
+  target_service = {
+    function_app = {
+      name = module.op_func.function_app.function_app.name
+    }
+  }
+
+  tags = var.tags
+}
