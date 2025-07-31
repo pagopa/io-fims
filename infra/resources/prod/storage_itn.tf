0f3e0f5572d0ea2dbce32e7d7fae1ebecd3aaa98
Roberto Cocco <robertococco@outlook.com>
2025-07-31 10:38:17 +0000

    Add storage account fims itn

---
+++ infra/resources/prod/storage_itn.tf
@@ -0,0 +1,19 @@
+module "storage_itn" {
+  source = "../_modules/storage_itn"
+
+  location            = module.itn_resource_group.location
+  project             = local.project
+  resource_group_name = module.itn_resource_group.name
+
+  virtual_network = data.azurerm_virtual_network.vnet_common
+  subnet_pep_id   = data.azurerm_subnet.pendpoints.id
+
+  environment = {
+    prefix    = local.prefix
+    env_short = local.env_short
+    location  = module.itn_resource_group.location
+    domain    = local.domain
+  }
+
+  tags = local.tags
+}
