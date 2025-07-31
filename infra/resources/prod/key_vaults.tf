8ef3439e37ed659efe64688c05e3db91cecad1fa
Luca Mancosu <luca.mancosu@it.ey.com>
2025-07-31 12:37:02 +0000

    add itn key vault

--- infra/resources/prod/key_vaults.tf
+++ infra/resources/prod/key_vaults.tf
@@ -8,3 +8,15 @@ module "key_vaults" {
 
   tags = local.tags
 }
+
+module "key_vaults_itn" {
+  source = "../_modules/key_vaults_itn"
+
+  resource_group_name = module.itn_resource_group.name
+  location            = module.itn_resource_group.location
+  prefix              = local.prefix
+  env_short           = local.env_short
+  project             = local.project_legacy
+  common_project      = local.common_project
+  tags                = local.tags
+}
