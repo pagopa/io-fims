881a470a67437c2ff883aa45c8d30436f8b3a6e0
Luca Mancosu <luca.mancosu@it.ey.com>
2025-08-01 13:41:23 +0000

    change dx azure_app_service ref

--- infra/resources/_modules/web_apps_itn/op_app.tf
+++ infra/resources/_modules/web_apps_itn/op_app.tf
@@ -23,7 +23,7 @@ locals {
 }
 
 module "op_app" {
-  source = "git::https://github.com/pagopa/dx.git//infra/modules/azure_app_service?ref=b3254c7da4b140070093c4af379b1d3670e4a3c2"
+  source = "git::https://github.com/pagopa/dx.git//infra/modules/azure_app_service?ref=5f795b96d84a866de514ab32199ba3f54286f702"
 
   environment = merge(var.environment, {
     app_name        = "op",
