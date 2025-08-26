214dae713bb92a93e088d8e8cebc26203719139c
Christian Calabrese <christian.calabrese@pagopa.it>
2025-08-26 17:18:52 +0200

    Remove identity and github runner and rename bootstrapper folder

--- infra/bootstrapper/prod/versions.tf
+++ infra/bootstrapper/prod/versions.tf
@@ -2,7 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3"
+      version = ">= 3.94.0"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
     }
   }
 
@@ -10,11 +15,15 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfappprodio"
     container_name       = "terraform-state"
-    key                  = "io-fims.github-runner.tfstate"
+    key                  = "io-fims.bootstrapper.prod.tfstate"
   }
 }
 
 provider "azurerm" {
   features {
   }
+}
+
+provider "github" {
+  owner = "pagopa"
 }
