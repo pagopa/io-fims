596d9b07f8b1bf7bdef6f5a3213b82dc12ec2300
Christian Calabrese <christian.calabrese@pagopa.it>
2025-08-27 11:28:42 +0200

    Fix plan and apply cicd workflows environments

--- infra/bootstrapper/prod/data.tf
+++ infra/bootstrapper/prod/data.tf
@@ -1,18 +1,3 @@
-data "azurerm_user_assigned_identity" "identity_prod_ci" {
-  name                = "io-p-fims-github-ci-identity"
-  resource_group_name = local.identity_resource_group_name
-}
-
-data "azurerm_user_assigned_identity" "identity_prod_cd" {
-  name                = "io-p-fims-github-cd-identity"
-  resource_group_name = local.identity_resource_group_name
-}
-
-data "azurerm_user_assigned_identity" "identity_app_prod_cd" {
-  name                = "io-p-fims-app-github-cd-identity"
-  resource_group_name = local.identity_resource_group_name
-}
-
 data "azurerm_key_vault" "fims" {
   name                = "io-p-fims-kv"
   resource_group_name = "io-p-fims-rg"
