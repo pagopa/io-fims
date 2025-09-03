1fdf72d149a284351d0b52fbf06fa159383f8891
Luca Mancosu <luca.mancosu@it.ey.com>
2025-09-03 12:31:43 +0000

    change key_vaults_itn name to key_vaults

--- infra/resources/prod/key_vaults.tf
+++ infra/resources/prod/key_vaults.tf
@@ -1,5 +1,5 @@
-module "key_vaults_itn" {
-  source = "../_modules/key_vaults_itn"
+module "key_vaults" {
+  source = "../_modules/key_vaults"
 
   resource_group_name = module.itn_resource_group.name
   location            = module.itn_resource_group.location
