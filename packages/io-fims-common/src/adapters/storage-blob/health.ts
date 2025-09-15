5a876916c58ace38f962d0a384ba794ce7060dc1
robbcocco <robertococco@outlook.com>
2025-09-15 14:33:15 +0200

    Add health check on storage blobs in op-func
    Add health check on storage blobs in op-func
    Remove unused cosmosdb references in op-func

---
+++ packages/io-fims-common/src/adapters/storage-blob/health.ts
@@ -0,0 +1,19 @@
+import type { HealthChecker } from "@/use-cases/health.js";
+
+import { BlobServiceClient } from "@azure/storage-blob";
+
+export class StorageBlobHealthChecker implements HealthChecker {
+  #client: BlobServiceClient;
+  id = "Azure Blob Storage";
+
+  constructor(blobClient: BlobServiceClient) {
+    this.#client = blobClient;
+  }
+
+  async health() {
+    const containers = await this.#client.listContainers().next();
+    if (containers.done) {
+      throw new Error("No containers found");
+    }
+  }
+}
