2795bf59f43bd7f91e1f0d02f7799df8864bbf9e
robbcocco <robertococco@outlook.com>
2025-09-15 15:25:37 +0200

    Add log on storage-blob health check
    Add log on storage-blob health check
    Add permission for staging slot for op-func

--- infra/resources/_modules/web_apps_itn/op_func.tf
+++ infra/resources/_modules/web_apps_itn/op_func.tf
@@ -61,12 +61,20 @@ resource "azurerm_role_assignment" "config_queue_op_func" {
 }
 
 resource "azurerm_role_assignment" "audit_event_container_op_func" {
+  for_each = toset([
+    module.op_func.function_app.function_app.principal_id,
+    module.op_func.function_app.function_app.slot.principal_id
+  ])
   scope                = var.audit_storage_fallback.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = module.op_func.function_app.function_app.principal_id
 }
 
 resource "azurerm_role_assignment" "audit_event_container_op_func_itn" {
+  for_each = toset([
+    module.op_func.function_app.function_app.principal_id,
+    module.op_func.function_app.function_app.slot.principal_id
+  ])
   scope                = var.audit_storage.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = module.op_func.function_app.function_app.principal_id
--- packages/io-fims-common/src/adapters/storage-blob/health.ts
+++ packages/io-fims-common/src/adapters/storage-blob/health.ts
@@ -1,19 +1,31 @@
 import type { HealthChecker } from "@/use-cases/health.js";
 
 import { BlobServiceClient } from "@azure/storage-blob";
+import { pino } from "pino";
 
 export class StorageBlobHealthChecker implements HealthChecker {
   #client: BlobServiceClient;
-  id = "Azure Blob Storage";
+  id: string;
+  logger: pino.Logger;
 
-  constructor(blobClient: BlobServiceClient) {
+  constructor(blobClient: BlobServiceClient, id = "Azure Blob Storage") {
     this.#client = blobClient;
+    this.id = id;
+
+    this.logger = pino({
+      level: process.env.NODE_ENV === "production" ? "error" : "debug",
+    });
   }
 
   async health() {
-    const containers = await this.#client.listContainers().next();
-    if (containers.done) {
-      throw new Error("No containers found");
+    try {
+      const containers = await this.#client.listContainers().next();
+      if (containers.done) {
+        throw new Error("No containers found");
+      }
+    } catch (e) {
+      this.logger.error("Health check failed", JSON.stringify(e));
+      throw e;
     }
   }
 }
