b9b943f6b2b5c3414fc7bb544016690ce3bb9230
Roberto Cocco <robertococco@outlook.com>
2025-09-15 10:59:30 +0000

    Add health check on storage blobs in op-func Remove cosmosdb from op-func

--- apps/op-func/src/func.ts
+++ apps/op-func/src/func.ts
@@ -7,16 +7,11 @@ import {
   httpAzureFunction,
 } from "@pagopa/handler-kit-azure-func";
 import { loadConfigFromEnvironment } from "io-fims-common/adapters/config";
-import { initCosmos } from "io-fims-common/adapters/cosmos/index";
+import { StorageBlobHealthChecker } from "io-fims-common/adapters/storage-blob/health";
 import healthHandler from "io-fims-common/adapters/handlers/health";
 import { HealthUseCase } from "io-fims-common/use-cases/health";
 
 import { Config, configFromEnvironment } from "./adapters/config.js";
-/*import { CosmosOIDCClientRepository } from "./adapters/cosmos/oidc-client.js";
-import {
-  createOIDCClientHandler,
-  inputDecoder,
-} from "./adapters/handlers/create-oidc-client.js";*/
 import {
   auditEventInputDecoder,
   manageAuditEventHandler,
@@ -24,9 +19,6 @@ import {
 import { BlobAuditEventRepository } from "./infra/storage/audit-event.js";
 
 async function main(config: Config) {
-  const cosmos = initCosmos(config.cosmos, new DefaultAzureCredential());
-  const health = new HealthUseCase([cosmos.healthChecker]);
-
   const blobServiceClient = new BlobServiceClient(
     config.auditEventStorage.uri,
     new DefaultAzureCredential(),
@@ -35,6 +27,11 @@ async function main(config: Config) {
     config.auditEventStorage.fallback,
     new DefaultAzureCredential(),
   );
+
+  const health = new HealthUseCase([
+    new StorageBlobHealthChecker(blobServiceClient),
+    new StorageBlobHealthChecker(legacyBlobServiceClient),
+  ]);
 
   const blobServiceClientWithFallback = new BlobServiceClientWithFallBack(
     blobServiceClient,
@@ -45,7 +42,6 @@ async function main(config: Config) {
     config.auditEventStorage.containerName,
   );
 
-  //const oidcClientRepository = new CosmosOIDCClientRepository(cosmos.database);
   const auditEventRepository = new BlobAuditEventRepository(containerClient);
 
   app.http("Health", {
@@ -55,15 +51,6 @@ async function main(config: Config) {
     methods: ["GET", "POST"],
     route: "health",
   });
-
-  /*app.storageQueue("CreateOIDCClient", {
-    connection: config.storage.connectionPrefix,
-    handler: azureFunction(createOIDCClientHandler)({
-      inputDecoder,
-      oidcClientRepository,
-    }),
-    queueName: config.storage.queue.config.name,
-  });*/
 
   app.storageQueue("ManageAuditEvent", {
     connection: config.storage.connectionPrefix,
