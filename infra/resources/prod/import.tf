f017d23b1d99d5aa68320fbb74a5e468fb527550
Roberto Cocco <robertococco@outlook.com>
2025-09-16 12:43:47 +0000

    Add import for op-func role assignment

---
+++ infra/resources/prod/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.web_apps_itn.azurerm_role_assignment.config_queue_op_func["Storage Queue Data Contributor"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-fims-rg/providers/Microsoft.Storage/storageAccounts/iopfimsst/providers/Microsoft.Authorization/roleAssignments/974c5e8b-45b9-4653-ba55-5f855dd0fb88"
+}
