693ac67d0eada6fdd18ac61ffc7017dd883ddbdc
Roberto Cocco <robertococco@outlook.com>
2025-09-16 13:24:29 +0000

    Remove import tf

--- infra/resources/prod/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.web_apps_itn.azurerm_role_assignment.config_queue_op_func["Storage Queue Data Contributor"]
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-fims-rg/providers/Microsoft.Storage/storageAccounts/iopfimsst/providers/Microsoft.Authorization/roleAssignments/fc1bc241-b117-41bc-80a3-eacdbd9d4771"
-}
