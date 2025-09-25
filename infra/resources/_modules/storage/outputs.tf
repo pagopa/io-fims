598868f075567e0acd7460c607974ecc01bf2f36
Roberto Cocco <robertococco@outlook.com>
2025-09-25 14:17:23 +0000

    Remove fims st weu

--- infra/resources/_modules/storage/outputs.tf
+++ infra/resources/_modules/storage/outputs.tf
@@ -1,17 +1,3 @@
-output "fims" {
-  value = {
-    id                  = module.storage_account_fims.id
-    name                = module.storage_account_fims.name
-    resource_group_name = module.storage_account_fims.resource_group_name
-    queues = {
-      config       = azurerm_storage_queue.config
-      access       = azurerm_storage_queue.access
-      export       = azurerm_storage_queue.export
-      audit_events = azurerm_storage_queue.audit_events
-    }
-  }
-}
-
 output "audit" {
   value = {
     id                  = module.audit_st.id
