905b22aca2c446a6a56a1d3f7665c1bb53422d3b
garma <64153597+Garma00@users.noreply.github.com>
2025-10-24 11:19:37 +0200

    Import new cosmos db user to terraform configuration
    - Import the new db user to the terraform configuration
    - Create accesses and export-requests containers into it
    
    Closes IOCOM-2633

--- infra/resources/_modules/cosmos/cosmos_database_user.tf
+++ infra/resources/_modules/cosmos/cosmos_database_user.tf
@@ -6,6 +6,44 @@ module "cosmosdb_database_user" {
   account_name        = module.cosmosdb_account_fims.name
 }
 
+resource "azurerm_cosmosdb_sql_database" "user" {
+  name                = "user"
+  resource_group_name = var.itn_resource_group_name
+  account_name        = module.azure-cosmos-account.name
+
+  autoscale_settings {
+    max_throughput = 10000
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "accesses" {
+  name                = "accesses"
+  resource_group_name = var.itn_resource_group_name
+
+  account_name          = module.azure-cosmos-account.name
+  database_name         = azurerm_cosmosdb_sql_database.user.name
+  partition_key_paths   = ["/fiscalCode"]
+  partition_key_version = 2
+
+  autoscale_settings {
+    max_throughput = 20000
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "export_requests" {
+  name                = "export-requests"
+  resource_group_name = var.itn_resource_group_name
+
+  account_name          = module.azure-cosmos-account.name
+  database_name         = azurerm_cosmosdb_sql_database.user.name
+  partition_key_paths   = ["/fiscalCode"]
+  partition_key_version = 2
+
+  autoscale_settings {
+    max_throughput = 20000
+  }
+}
+
 module "comsosdb_sql_container_user_export_requests" {
   source = "github.com/pagopa/terraform-azurerm-v3//cosmosdb_sql_container?ref=v8.28.2"
 
