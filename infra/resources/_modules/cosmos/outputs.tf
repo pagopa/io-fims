f937bb49001ded845e9a697435505f0efc4baf23
garma <giovanniarmaleo00@gmail.com>
2025-10-27 12:39:04 +0100

    Migrate web_apps to new cosmos account

--- infra/resources/_modules/cosmos/outputs.tf
+++ infra/resources/_modules/cosmos/outputs.tf
@@ -1,6 +1,6 @@
 output "fims" {
   value = {
-    name                = module.cosmosdb_account_fims.name
-    resource_group_name = var.resource_group_name
+    name                = module.azure-cosmos-account.name
+    resource_group_name = var.itn_resource_group_name
   }
 }
