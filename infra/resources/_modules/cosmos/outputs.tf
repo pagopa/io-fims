57c27225b611abcda933fc124a798be924966ef3
garma <64153597+Garma00@users.noreply.github.com>
2025-10-30 12:25:13 +0100

    Make op-app use the new cosmos

--- infra/resources/_modules/cosmos/outputs.tf
+++ infra/resources/_modules/cosmos/outputs.tf
@@ -4,3 +4,10 @@ output "fims" {
     resource_group_name = var.resource_group_name
   }
 }
+
+output "fims_itn" {
+  value = {
+    name                = module.azure-cosmos-account.name
+    resource_group_name = var.itn_resource_group_name
+  }
+}
