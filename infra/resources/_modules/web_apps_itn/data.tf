89a321167a392d3b53cedddbbcbd58a131d41a9c
robbcocco <robertococco@outlook.com>
2025-11-10 11:42:08 +0100

    Remove fims st from WEU
    Closes IOCOM-2610
    
    ---------
    
    Co-authored-by: garma <giovanniarmaleo00@gmail.com>

--- infra/resources/_modules/web_apps_itn/data.tf
+++ infra/resources/_modules/web_apps_itn/data.tf
@@ -1,5 +1,3 @@
-data "azurerm_client_config" "current" {}
-
 data "azurerm_cosmosdb_account" "fims" {
   name                = var.cosmosdb_account.name
   resource_group_name = var.cosmosdb_account.resource_group_name
@@ -20,11 +18,6 @@ data "azurerm_cosmosdb_sql_database" "fims_op" {
   name                = "op"
   resource_group_name = var.cosmosdb_account.resource_group_name
   account_name        = var.cosmosdb_account.name
-}
-
-data "azurerm_storage_account" "fims" {
-  name                = var.storage.name
-  resource_group_name = var.storage.resource_group_name
 }
 
 data "azurerm_storage_account" "fims_itn" {
